7055634fb458fa3b5199d23ef056cae9ed01c830
SAI SHANKAR <80621346+galisaishankar08@users.noreply.github.com>
2022-10-23 10:43:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,18 +16,17 @@ app.config['IMAGE_UPLOADS'] = UPLOAD_FOLDER
 def detect(fname):
     filename= '.\\uploads\\'+fname
     result = []
-    try:
-        img=image.load_img(filename,target_size=(224,224))
-        mobile =tf.keras.applications.mobilenet.MobileNet()
-        resized_img=image.img_to_array(img)
-        final_image=np.expand_dims(resized_img,axis=0)
-        final_image=tf.keras.applications.mobilenet.preprocess_input(final_image)
-        pred=mobile.predict(final_image)
-        print(pred)
-        result=imagenet_utils.decode_predictions(pred)
-        return result
-    except:
-        return -1
+#     try:
+    img=image.load_img(filename,target_size=(224,224))
+    mobile =tf.keras.applications.mobilenet.MobileNet()
+    resized_img=image.img_to_array(img)
+    final_image=np.expand_dims(resized_img,axis=0)
+    final_image=tf.keras.applications.mobilenet.preprocess_input(final_image)
+    pred=mobile.predict(final_image)
+    result=imagenet_utils.decode_predictions(pred)
+    return result
+#     except:
+#         return -1
 
 
     
@@ -43,20 +42,20 @@ def uploadimg():
         
         file1 = request.files['image']
         path = os.path.join(app.config['IMAGE_UPLOADS'], file1.filename)
-        try:
-            file1.save(path)
-            out = []
-            
-            out = detect(file1.filename)
+#         try:
+        file1.save(path)
+        out = []
+          
+        out = detect(file1.filename)
 
-            if out!=-1:
-                for i in range(len(out[0])):
-                    print(out[0][i][1])
-                return render_template('result.html', results=out, n=len(out[0]))
-            else:
-                return redirect('/')
-        except:
+        if out!=-1:
+            for i in range(len(out[0])):
+                print(out[0][i][1])
+            return render_template('result.html', results=out, n=len(out[0]))
+        else:
             return redirect('/')
+#         except:
+#             return redirect('/')
 
 
 if __name__ == '__main__':
